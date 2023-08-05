bae62a042f4b452cd20b7480bb86b710984bc478
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-05 21:03:29 +0900

    feat: mouseOn,Out 시 BackDrop 이벤트 처리

--- src/pages/ProjectPage/ProjectCard.tsx
+++ src/pages/ProjectPage/ProjectCard.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import projectImage from '~/assets/images/projectCardImg.png';
 
 function ProjectCard() {
+  const [isMouseOn, setIsMouseOn] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsMouseOn(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsMouseOn(false);
+  };
+
   return (
-    <Container>
+    <Container onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <CardSection src={projectImage} />
+      {isMouseOn && <CardBackDrop />}
     </Container>
   );
 }
@@ -19,4 +31,13 @@ const Container = styled.article`
 const CardSection = styled.img`
   width: 100%;
   height: 100%;
+`;
+const CardBackDrop = styled.article`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
 `;
