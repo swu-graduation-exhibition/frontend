4607d2f31d912ca6ed1366dc0e0dfda0da6615ef
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-05 22:22:51 +0900

    data: ProjectCardData dummy 생성

--- src/pages/ProjectPage/data/cardData.ts
+++ src/pages/ProjectPage/data/cardData.ts
@@ -1,11 +1,70 @@
-export const projectData: TProjectData = {
-  category: 'UX',
-  title: '프로젝트 타이틀 Title 123',
-  participants: ['팀원1', '팀원2', '팀원3', '팀원4', '팀원5'],
-};
-
-type TProjectData = {
+export interface ProjectData {
   category: string;
   title: string;
   participants: string[];
-};
+}
+
+const projectData: ProjectData[] = [
+  {
+    category: 'UX',
+    title: '프로젝트 1',
+    participants: ['팀원1', '팀원2', '팀원3', '팀원4', '팀원5'],
+  },
+  {
+    category: 'UX',
+    title: '웹 프로젝트 2',
+    participants: ['팀원6', '팀원7', '팀원8'],
+  },
+  {
+    category: 'UX',
+    title: '모바일 앱 3',
+    participants: ['팀원9', '팀원10', '팀원11', '팀원12'],
+  },
+  {
+    category: 'UX',
+    title: '데이터 분석 프로젝트 4',
+    participants: ['팀원13', '팀원14', '팀원15'],
+  },
+  {
+    category: 'UX',
+    title: '인공지능 프로젝트 5',
+    participants: ['팀원16', '팀원17'],
+  },
+  {
+    category: 'UX',
+    title: '모바일 앱 6',
+    participants: ['팀원18', '팀원19', '팀원20'],
+  },
+  {
+    category: 'UX',
+    title: '웹 프로젝트 7',
+    participants: ['팀원21', '팀원22', '팀원23', '팀원24'],
+  },
+  {
+    category: 'UX',
+    title: '데이터 분석 프로젝트 8',
+    participants: ['팀원25', '팀원26', '팀원27'],
+  },
+  {
+    category: 'UX',
+    title: '프로젝트 9',
+    participants: ['팀원28', '팀원29', '팀원30', '팀원31'],
+  },
+  {
+    category: 'UX',
+    title: '인공지능 프로젝트 10',
+    participants: ['팀원32', '팀원33', '팀원34'],
+  },
+  {
+    category: 'UX',
+    title: '웹 프로젝트 11',
+    participants: ['팀원35', '팀원36', '팀원37'],
+  },
+  {
+    category: 'UX',
+    title: '모바일 앱 12',
+    participants: ['팀원38', '팀원39', '팀원40', '팀원41'],
+  },
+];
+
+export default projectData;
