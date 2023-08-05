91c2e5914cb8f243ac8f1ddd3bf2368ff54ea3ac
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-05 22:19:31 +0900

    data: project Dummy Data 생성

---
+++ src/pages/ProjectPage/data/cardData.ts
@@ -0,0 +1,11 @@
+export const projectData: TProjectData = {
+  category: 'UX',
+  title: '프로젝트 타이틀 Title 123',
+  participants: ['팀원1', '팀원2', '팀원3', '팀원4', '팀원5'],
+};
+
+type TProjectData = {
+  category: string;
+  title: string;
+  participants: string[];
+};
