19b88951e1dbb4f46252183e7d8c2bb87162f4ca
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-05 22:20:29 +0900

    design: dummyData 로 ProjcecCard 퍼블리싱

--- src/pages/ProjectPage/ProjectCard.tsx
+++ src/pages/ProjectPage/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import projectImage from '~/assets/images/projectCardImg.png';
+import { projectData } from './data/cardData';
+import theme from '~/styles/theme';
 
 function ProjectCard() {
   const [isMouseOn, setIsMouseOn] = useState(false);
@@ -13,10 +15,24 @@ function ProjectCard() {
     setIsMouseOn(false);
   };
 
+  const { category, title, participants } = projectData;
+
   return (
     <Container onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <CardSection src={projectImage} />
-      {isMouseOn && <CardBackDrop />}
+      {isMouseOn && (
+        <CardBackDrop>
+          <CategoryText>{category}</CategoryText>
+          <ProjectInfoContainer>
+            <TitleText>{title}</TitleText>
+            <ParticipantsContainer>
+              {participants.map((member, i) => {
+                return <MemberText key={i}>{member}</MemberText>;
+              })}
+            </ParticipantsContainer>
+          </ProjectInfoContainer>
+        </CardBackDrop>
+      )}
     </Container>
   );
 }
@@ -40,4 +56,38 @@ const CardBackDrop = styled.article`
   top: 0;
   left: 0;
   background: rgba(0, 0, 0, 0.4);
+  animation: fadeInAnimation 0.5s ease-in-out forwards;
+  @keyframes fadeInAnimation {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const CategoryText = styled.span`
+  color: white;
+  ${({ theme }) => theme.fonts.Body_03}
+
+  position: absolute;
+  top: 3.2rem;
+  right: 3.2rem;
+`;
+const TitleText = styled.span`
+  color: white;
+  ${({ theme }) => theme.fonts.Body_03}
+`;
+
+const ProjectInfoContainer = styled.section`
+  position: absolute;
+  bottom: 3.2rem;
+  left: 3.2rem;
+`;
+const ParticipantsContainer = styled.div``;
+
+const MemberText = styled.span`
+  color: white;
+  ${({ theme }) => theme.fonts.Caption_02}
 `;
