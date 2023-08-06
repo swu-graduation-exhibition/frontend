eed54c39febd945cd4ffe42110b6b0426505df2d
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-06 19:07:37 +0900

    design: 버튼 반응형 UI 적용

--- src/pages/ProjectPage/components/ProjectCategory.tsx
+++ src/pages/ProjectPage/components/ProjectCategory.tsx
@@ -28,9 +28,9 @@ function ProjectCategory() {
               key={i}
               isChecked={isSelected === category}
               onClick={() => handleCategory(category)}
-              onMouseOver={() => handleMouseOver(category)}
-              onMouseOut={handleMouseOut}
-              isMouseOver={isMouseOver === category}
+              // onMouseOver={() => handleMouseOver(category)}
+              // onMouseOut={handleMouseOut}
+              // isMouseOver={isMouseOver === category}
             >
               {category}
             </CategoryButton>
@@ -52,10 +52,19 @@ const Container = styled.section`
   border-bottom: 1px solid black;
 
   padding: 16rem 0 8rem;
+
+  @media screen and (width <= 1400px) {
+    justify-content: flex-start;
+    height: fit-content;
+    padding: 1.3rem 0 5.2rem 2.9rem;
+    flex-direction: column;
+    gap: 1.8rem;
+  }
 `;
 
 const NavTitle = styled.header`
   ${({ theme }) => theme.fonts.Headline_03}
+  width: fit-content;
   height: fit-content;
 `;
 
@@ -63,30 +72,34 @@ const ButtonSection = styled.div`
   display: flex;
   flex-direction: row;
   gap: 1.6rem;
+  width: fit-content;
+
+  @media screen and (width <= 1400px) {
+    flex-direction: column;
+  }
 `;
 
 const CategoryButton = styled.button<{ isChecked: boolean; isMouseOver: boolean }>`
   padding: 1.6rem 3.2rem;
-  border: 1.5px solid var(--grayscales-800, #343a40);
+  border: 0.15rem solid var(--grayscales-800, #343a40);
+  min-width: fit-content;
 
-  background-color: ${({ isMouseOver, isChecked, theme }) =>
-    isMouseOver
-      ? 'rgba(134, 142, 150, 0.10)'
-      : isChecked
-      ? theme.colors.Grayscales_900
-      : theme.colors.White};
-  color: ${({ isMouseOver, isChecked, theme }) =>
-    isMouseOver
-      ? theme.colors.Grayscales_700
-      : isChecked
-      ? theme.colors.White
-      : theme.colors.Grayscales_700};
+  background-color: ${({ isChecked, theme }) =>
+    isChecked ? theme.colors.Grayscales_900 : theme.colors.White};
+  color: ${({ isChecked, theme }) =>
+    isChecked ? theme.colors.White : theme.colors.Grayscales_700};
 
   text-align: center;
   font-family: 'Antic Didone', serif;
   font-size: 3.2rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 40px;
+  font-weight: 500;
   border-radius: 0.4rem;
+
+  @media screen and (width <= 1400px) {
+    font-size: 2.1rem;
+    font-family: 'Antic Didone', serif;
+
+    text-align: start;
+    font-weight: 500;
+  }
 `;
