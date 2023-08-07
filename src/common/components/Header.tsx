8edbfdfb6c46cfb164b1e28ce37ebd9fd8a1474c
seohee0112 <ghdtj7@naver.com>
2023-08-07 10:38:58 +0900

    feat : 반응형 헤더 구현

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,21 +1,41 @@
+import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { styled } from 'styled-components';
-import { IcDefaultBar, IcHeaderLogo } from '~/assets/icons';
-import { PAGE_CATEGORIES, PAGE_PATHS } from '~/constants/common';
+import { css, styled } from 'styled-components';
+import { IcDefaultBar, IcHeaderLogo, IcCloseBar } from '~/assets/icons';
+import {
+  PAGE_CATEGORIES,
+  PAGE_PATHS,
+  MOBILE_PAGE_CATEGORIES,
+  MOBILE_PAGE_PATHS,
+} from '~/constants/common';
 import useWindowSize from '~/hooks/useWindowSize';
 
 const Header = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const { width } = useWindowSize();
+  const [isToggle, setIsToggle] = useState(false);
 
   return (
-    <HeaderWrapper>
+    <HeaderWrapper isToggle={isToggle}>
       <div>
-        <IcHeaderLogo onClick={() => navigate('/')} />
+        <IcHeaderLogo onClick={() => navigate('/')} />{' '}
       </div>
       {width <= 640 ? (
-        <IcDefaultBar />
+        isToggle ? (
+          <>
+            <IcCloseBar onClick={() => setIsToggle(false)} />
+            <MobileLinkWrapper>
+              {MOBILE_PAGE_CATEGORIES.map((category, idx) => (
+                <LiContent key={category}>
+                  <Link to={MOBILE_PAGE_PATHS[idx]}>{category}</Link>
+                </LiContent>
+              ))}
+            </MobileLinkWrapper>
+          </>
+        ) : (
+          <IcDefaultBar onClick={() => setIsToggle(true)} />
+        )
       ) : (
         <LinkWrapper>
           {PAGE_CATEGORIES.map((category, idx) => (
@@ -36,24 +56,25 @@ const Header = () => {
 
 export default Header;
 
-const HeaderWrapper = styled.div`
+const HeaderWrapper = styled(({ isToggle, ...props }) => <div {...props} />)`
   display: flex;
   align-items: center;
   justify-content: space-between;
   max-width: 192rem;
   height: 11rem;
-  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.Black};
-  padding: 0 10%;
+  ${({ isToggle }) =>
+    isToggle && window.innerWidth <= 630
+      ? css`
+          border-bottom: 0.1rem solid ${({ theme }) => theme.colors.White};
+          background-color: ${({ theme }) => theme.colors.Black};
+          opacity: 70%;
 
-  div > svg {
-    cursor: pointer;
-    @media screen and (width <= 1400px) {
-      /* height: 8rem; */
-    }
-    @media screen and (width <= 830px) {
-      height: 2.6rem;
-    }
-  }
+          position: relative;
+        `
+      : css`
+          border-bottom: 0.1rem solid ${({ theme }) => theme.colors.Black};
+        `}
+  padding: 0 10%;
 
   @media screen and (width <= 1400px) {
     height: 8rem;
@@ -61,7 +82,28 @@ const HeaderWrapper = styled.div`
   }
   @media screen and (width <= 830px) {
     height: 5.5rem;
-    padding: 0 5%;
+    padding: 0 1.6rem 0 0;
+  }
+  svg {
+    cursor: pointer;
+    @media screen and (width <= 1400px) {
+      /* height: 8rem; */
+    }
+    @media screen and (width <= 830px) {
+      height: 2.6rem;
+    }
+    ${({ isToggle }) =>
+      isToggle && window.innerWidth <= 630
+        ? css`
+            path {
+              fill: white;
+            }
+          `
+        : css`
+            path {
+              fill: black;
+            }
+          `}
   }
 `;
 const LinkWrapper = styled.ul`
@@ -87,5 +129,29 @@ const LiContent = styled.li`
   }
   .incorrect {
     color: ${({ theme }) => theme.colors.Grayscales_500};
+  }
+`;
+const MobileLinkWrapper = styled(LinkWrapper)`
+  flex-direction: column;
+  gap: 0.3rem;
+
+  width: 100%;
+  position: absolute;
+  top: 5.5rem;
+  left: 0;
+  height: 100vh;
+  z-index: 2;
+
+  background-color: ${({ theme }) => theme.colors.Black};
+  opacity: 70%;
+
+  padding-left: 1.6rem;
+
+  a {
+    font-size: 30px;
+    font-weight: 300;
+    line-height: 46px;
+
+    color: ${({ theme }) => theme.colors.White};
   }
 `;
