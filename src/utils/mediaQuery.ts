677e43761f9c17edb4ec09ffd90d92532d461130
seohee0112 <ghdtj7@naver.com>
2023-08-09 18:02:31 +0900

    feat : mediaQuery utils 생성

---
+++ src/utils/mediaQuery.ts
@@ -0,0 +1,24 @@
+import { useMediaQuery } from 'react-responsive';
+
+const Desktop = ({ children }: { children: JSX.Element }): JSX.Element | null => {
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  return isDesktop ? children : null;
+};
+
+const Tablet = ({ children }: { children: JSX.Element }): JSX.Element | null => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+  return isTablet ? children : null;
+};
+
+const Mobile = ({ children }: { children: JSX.Element }): JSX.Element | null => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+
+// mobile이 아닐 때만 출력되는 컴포넌트
+const Default = ({ children }: { children: JSX.Element }): JSX.Element | null => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 });
+  return isNotMobile ? children : null;
+};
+
+export { Desktop, Tablet, Mobile, Default };
