2d88b928162c3d0e55a7f214172b49f44b30285e
seohee0112 <ghdtj7@naver.com>
2023-08-10 18:59:51 +0900

    fix : 홈배너로인한 이미지 배경 절대 위치 조정을 위한 props 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import { PageLayout } from '~/common/components';
-import { ConceptContent, Interview, MainBanner } from './components';
-import ImgGraduationExhibition from '~/assets/images/home_middle_background.png';
-import ImgMobileGraduationExhibition from '~/assets/images/bg1_all_mobile.png';
+import { ConceptContent, Interview, MainBanner, TrackIntroduce } from './components';
 import ImgBlackFlower from '~/assets/images/img2_home.png';
 import ImgMobileBlackFlower from '~/assets/images/img2_home_mobile.png';
 import { MOBILE_WIDTH } from '~/constants/common';
@@ -11,9 +9,8 @@ import { Default, Mobile } from '~/utils/mediaQuery';
 
 const Home = () => {
   return (
-    <PageLayout>
+    <PageLayout mainBanner={<MainBanner />}>
       <HomeWrapper>
-        <MainBanner />
         <GraduationExhibitionWrapper>
           <ConceptContent />
           <Default>
@@ -23,6 +20,7 @@ const Home = () => {
             <MobileBlackFlower src={ImgMobileBlackFlower} />
           </Mobile>
           <Interview />
+          <TrackIntroduce />
         </GraduationExhibitionWrapper>
       </HomeWrapper>
     </PageLayout>
@@ -34,21 +32,14 @@ export default Home;
 const HomeWrapper = styled.div`
   /* display: flex; */
   /* flex-direction: column; */
-  position: relative;
 `;
 
 const GraduationExhibitionWrapper = styled.section`
   position: relative;
 
-  height: 251.9rem;
-  background-image: url(${ImgGraduationExhibition});
-  background-size: cover;
-  background-repeat: no-repeat;
-
-  padding: 3% 10%;
+  /* height: 251.9rem; */
+  padding: 3% 0 3% 10%;
   @media screen and (width <= ${MOBILE_WIDTH}) {
-    background-image: url(${ImgMobileGraduationExhibition});
-
     padding: 2rem 1.6rem;
   }
 `;
