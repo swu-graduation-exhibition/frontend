765dd42599c69fd4910ceff29ca73585ce09af22
seohee0112 <ghdtj7@naver.com>
2023-08-10 18:58:59 +0900

    style : 홈에 필요한 아이콘추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as IcHeaderLogo } from './ic_header_logo.svg';
 export { ReactComponent as IcDefaultBar } from './ic_default_bar.svg';
 export { ReactComponent as IcCloseBar } from './x.svg';
+export { ReactComponent as IcArrowHome } from './ic_arrow_home.svg';
