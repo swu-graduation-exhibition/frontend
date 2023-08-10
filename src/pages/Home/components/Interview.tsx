f9a6cf883850daac59e64ace70642dce3dc71a1f
seohee0112 <ghdtj7@naver.com>
2023-08-10 14:53:29 +0900

    feat : interview 컴포넌트 구현

---
+++ src/pages/Home/components/Interview.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import { MOBILE_WIDTH } from '~/constants/common';
+import YoutubeList from './YoutubeList';
+
+const Interview = () => {
+  return (
+    <InterviewWrapper>
+      <Title>2023 졸업 작품 전시회 인터뷰</Title>
+      <YoutubeList />
+    </InterviewWrapper>
+  );
+};
+
+export default Interview;
+
+const InterviewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 16.7rem;
+
+  @media screen and (width <= ${MOBILE_WIDTH}) {
+    margin-top: 3.4rem;
+  }
+`;
+
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.Headline_03};
+
+  @media screen and (width <= ${MOBILE_WIDTH}) {
+    ${({ theme }) => theme.fonts.Mobile_Headline_01};
+    margin-top: 3.4rem;
+  }
+`;
--- src/pages/Home/components/index.ts
+++ src/pages/Home/components/index.ts
@@ -2,3 +2,4 @@ export { default as MainBanner } from './MainBanner';
 export { default as UniversityContent } from './UniversityContent';
 export { default as LocationContent } from './LocationContent';
 export { default as ConceptContent } from './ConceptContent';
+export { default as Interview } from './Interview';
