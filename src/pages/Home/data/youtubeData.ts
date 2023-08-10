c2fb0c95a315681a25c85f63978f934145efd90b
seohee0112 <ghdtj7@naver.com>
2023-08-10 14:51:59 +0900

    docs : youtube 목 데이터 추가

---
+++ src/pages/Home/data/youtubeData.ts
@@ -0,0 +1,8 @@
+import { YoutubeDataInfo } from '~/types/home';
+
+export const youtubeData: YoutubeDataInfo[] = [
+  { id: 1, url: 'https://www.youtube.com/watch?v=f6X36lcmg6M&t=5576s' },
+  { id: 2, url: 'https://www.youtube.com/watch?v=f6X36lcmg6M&t=5576s' },
+  { id: 3, url: 'https://www.youtube.com/watch?v=f6X36lcmg6M&t=5576s' },
+  { id: 4, url: 'https://www.youtube.com/watch?v=f6X36lcmg6M&t=5576s' },
+];
