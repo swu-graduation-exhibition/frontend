368576f00dbec6407da5aadd040944484d12a720
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-13 14:51:56 +0900

    design: ProjectDetailHeader Container testCode

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,5 +1,22 @@
+import { PageLayout } from '~/common/components';
+import styled from 'styled-components';
+import ProjectDetailHeader from './components/ProjectDetailHeader';
+
+const ProjectDetailContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 13.6rem;
+  border: 1px solid black;
+`;
+
 function ProjectDetail() {
-  return <div>ProjectDetail</div>;
+  return (
+    <PageLayout>
+      <ProjectDetailContainer>
+        <ProjectDetailHeader />
+      </ProjectDetailContainer>
+    </PageLayout>
+  );
 }
 
 export default ProjectDetail;
---
+++ src/pages/ProjectDetail/components/ProjectDetailHeader.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50.2rem;
+
+  background-color: aqua;
+`;
+
+function ProjectDetailHeader() {
+  return <Container>ProjectDetailHeader</Container>;
+}
+
+export default ProjectDetailHeader;
