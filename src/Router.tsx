a6726fd61e7a15d3b32200df12fa0740759ff360
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-13 14:36:03 +0900

    feat: projectDetail Page Router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Project from './pages/ProjectPage/Project';
 import Home from './pages/Home/Home';
+import ProjectDetail from './pages/ProjectDetail/ProjectDetail';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/project/:category" element={<Project />} />
+        <Route path="/project/detail/:pojectId" element={<ProjectDetail />} />
       </Routes>
     </BrowserRouter>
   );
