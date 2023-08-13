7ec9dc2a391ec196b671d8ec431fb4b541057670
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-13 15:55:09 +0900

    design: ProcjectDetailHeader 틀 완성

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,12 +1,17 @@
 import { PageLayout } from '~/common/components';
 import styled from 'styled-components';
 import ProjectDetailHeader from './components/ProjectDetailHeader';
+import projectBgImg from '../../assets/images/projectBgImg.png';
 
 const ProjectDetailContainer = styled.div`
   width: 100%;
   height: 100vh;
   padding: 0 13.6rem;
   border: 1px solid black;
+
+  background-image: url(${projectBgImg});
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 function ProjectDetail() {
