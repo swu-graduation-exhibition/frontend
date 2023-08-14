01b76d2d1e19d36ec2f475d58fd6288bc6a2a52d
seohee0112 <ghdtj7@naver.com>
2023-08-14 14:43:13 +0900

    fix : useDragScroll 로 가로 스크롤 컴포넌트 변경

--- src/pages/Home/components/TrackIntroduce.tsx
+++ src/pages/Home/components/TrackIntroduce.tsx
@@ -1,61 +1,16 @@
-import { styled, css } from 'styled-components';
+import { styled } from 'styled-components';
 import { trackCardData } from '../data/trackData';
 import { TrackCard } from '.';
-import { useState } from 'react';
 import { MOBILE_WIDTH } from '~/constants/common';
-import { useCarouselSize } from '~/hooks/useCarouselSize';
-import { modifySliderRange, sliderEvent } from '~/utils/sliderEvent';
-import {
-  TRACK_INTRODUCE_MOBILE_SLIDER_ITEM_HALF_WIDTH,
-  TRACK_INTRODUCE_SLIDER_HALF_WIDTH,
-} from '~/constants/home';
-const TrackIntroduce = () => {
-  const [currentIdx, setCurrentIdx] = useState<number>(0);
-  const [transX, setTransX] = useState<number>(0);
-  const { ref } = useCarouselSize();
+import useDragScroll from '~/hooks/useDragScroll';
 
-  const width =
-    window.innerWidth > 767
-      ? TRACK_INTRODUCE_SLIDER_HALF_WIDTH * 2
-      : TRACK_INTRODUCE_MOBILE_SLIDER_ITEM_HALF_WIDTH * 2;
-  const sliderItemHalfWidth =
-    window.innerWidth > 767
-      ? TRACK_INTRODUCE_SLIDER_HALF_WIDTH
-      : TRACK_INTRODUCE_MOBILE_SLIDER_ITEM_HALF_WIDTH;
+const TrackIntroduce = () => {
+  const ref = useDragScroll();
 
   return (
     <TrackIntroduceWrapper>
       <DragListWrapper ref={ref}>
-        <DragSlider
-          currentIdx={currentIdx}
-          dragItemWidth={width}
-          transX={transX}
-          {...sliderEvent({
-            onDragChange: (deltaX) => {
-              setTransX(modifySliderRange(deltaX, -width, width));
-            },
-            onDragEnd: (deltaX) => {
-              const maxIndex = trackCardData.length - 1;
-
-              Array(maxIndex + 1)
-                .fill(0)
-                .map((_, i) => i + 1)
-                .some((num) => {
-                  console.log(currentIdx, num);
-                  if (deltaX < -sliderItemHalfWidth * num) {
-                    setCurrentIdx(modifySliderRange(currentIdx + num, 0, maxIndex));
-                    return true;
-                  }
-                  if (deltaX > sliderItemHalfWidth * num) {
-                    setCurrentIdx(modifySliderRange(currentIdx - num, 0, maxIndex));
-                    return true;
-                  }
-                });
-
-              setTransX(0);
-            },
-          })}
-        >
+        <DragSlider>
           {trackCardData.map(({ title, desc, url }) => (
             <TrackCard title={title} desc={desc} url={url} key={title} />
           ))}
@@ -77,20 +32,13 @@ const TrackIntroduceWrapper = styled.div`
 const DragListWrapper = styled.article`
   overflow: hidden;
 `;
-const DragSlider = styled(({ currentIdx, dragItemWidth, transX, ...props }) => (
-  <section {...props} />
-))`
+const DragSlider = styled.section`
   display: flex;
   align-items: center;
   gap: 2rem;
 
   width: 164.8rem;
-  ${({ currentIdx, dragItemWidth, transX }) => css`
-    transform: translateX(${(-currentIdx * dragItemWidth + transX) / 10}rem);
-  `};
-  ${({ transX }) => css`
-    transition: transform ${transX ? 0 : 300}ms ease-out 0s;
-  `};
+
   @media screen and (width <= ${MOBILE_WIDTH}) {
     width: 84.8rem;
     gap: 1rem;
--- src/pages/Home/components/YoutubeList.tsx
+++ src/pages/Home/components/YoutubeList.tsx
@@ -1,57 +1,15 @@
-import { styled, css } from 'styled-components';
-import { useState } from 'react';
+import { styled } from 'styled-components';
 import { youtubeData } from '../data/youtubeData';
 import { MOBILE_WIDTH } from '~/constants/common';
-import { useCarouselSize } from '~/hooks/useCarouselSize';
-import { modifySliderRange, sliderEvent } from '~/utils/sliderEvent';
-import {
-  YOUTUBE_MOBILE_SLIDER_ITEM_HALF_WIDTH,
-  YOUTUBE_SLIDER_ITEM_HALF_WIDTH,
-} from '~/constants/home';
+import useDragScroll from '~/hooks/useDragScroll';
 
 const YoutubeList = () => {
-  const [currentIdx, setCurrentIdx] = useState<number>(0);
-  const [transX, setTransX] = useState<number>(0);
-  const { ref } = useCarouselSize();
-  const width = window.innerWidth > 767 ? 816 : 214;
-  const sliderItemHalfWidth =
-    window.innerWidth > 767
-      ? YOUTUBE_SLIDER_ITEM_HALF_WIDTH
-      : YOUTUBE_MOBILE_SLIDER_ITEM_HALF_WIDTH;
+  const ref = useDragScroll();
 
   return (
     <YoutubeListWrapper>
       <DragListWrapper ref={ref}>
-        <DragSlider
-          currentIdx={currentIdx}
-          dragItemWidth={width}
-          transX={transX}
-          {...sliderEvent({
-            onDragChange: (deltaX) => {
-              setTransX(modifySliderRange(deltaX, -width, width));
-            },
-            onDragEnd: (deltaX) => {
-              const maxIndex = youtubeData.length - 1;
-
-              Array(maxIndex + 1)
-                .fill(0)
-                .map((_, i) => i + 1)
-                .some((num) => {
-                  console.log(currentIdx, num);
-                  if (deltaX < -sliderItemHalfWidth * num) {
-                    setCurrentIdx(modifySliderRange(currentIdx + num, 0, maxIndex));
-                    return true;
-                  }
-                  if (deltaX > sliderItemHalfWidth * num) {
-                    setCurrentIdx(modifySliderRange(currentIdx - num, 0, maxIndex));
-                    return true;
-                  }
-                });
-
-              setTransX(0);
-            },
-          })}
-        >
+        <DragSlider>
           {youtubeData.map(({ id, url }, idx) => (
             <YoutubeCard src="" key={id} alt={id + '번재'} />
           ))}
@@ -82,20 +40,13 @@ const YoutubeCard = styled.img`
     height: 12rem;
   }
 `;
-const DragSlider = styled(({ currentIdx, dragItemWidth, transX, ...props }) => (
-  <section {...props} />
-))`
+const DragSlider = styled.section`
   display: flex;
   align-items: center;
   gap: 1.6rem;
 
   width: 331.2rem;
-  ${({ currentIdx, dragItemWidth, transX }) => css`
-    transform: translateX(${(-currentIdx * dragItemWidth + transX) / 10}rem);
-  `};
-  ${({ transX }) => css`
-    transition: transform ${transX ? 0 : 300}ms ease-out 0s;
-  `};
+
   @media screen and (width <= ${MOBILE_WIDTH}) {
     width: 86.5rem;
   }
