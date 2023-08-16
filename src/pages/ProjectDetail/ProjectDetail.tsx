8078afa419357ae4ca53a2e3fabcde4752a0cc83
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-17 01:03:43 +0900

    design : ProjectDetailAssets 컴포넌트 dummy Container 적용

--- src/pages/ProjectDetail/ProjectDetail.tsx
+++ src/pages/ProjectDetail/ProjectDetail.tsx
@@ -1,16 +1,12 @@
 import { PageLayout } from '~/common/components';
 import styled from 'styled-components';
 import ProjectDetailHeader from './components/ProjectDetailHeader';
-import projectBgImg from '../../assets/images/projectBgImg.png';
+import ProjectDetailAssets from './components/ProjectDetailAssets';
 
 const ProjectDetailContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   padding: 0 13.6rem;
-
-  background-image: url(${projectBgImg});
-  background-size: cover;
-  background-repeat: no-repeat;
 `;
 
 function ProjectDetail() {
@@ -18,6 +14,7 @@ function ProjectDetail() {
     <PageLayout>
       <ProjectDetailContainer>
         <ProjectDetailHeader />
+        <ProjectDetailAssets />
       </ProjectDetailContainer>
     </PageLayout>
   );
