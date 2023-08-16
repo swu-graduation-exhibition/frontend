175806b09168ce2c8ae55a566769ae8dda19450b
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-17 01:03:43 +0900

    design : ProjectDetailAssets 컴포넌트 dummy Container 적용

---
+++ src/pages/ProjectDetail/components/ProjectDetailAssets.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ProjectDetailAssets = () => {
+  return <Container>ProjectDetailAssets</Container>;
+};
+
+export default ProjectDetailAssets;
+
+const Container = styled.section`
+  width: 100%;
+  height: 140rem;
+  margin-bottom: 20rem;
+  border: 1px solid salmon;
+
+  background-color: gray;
+`;
