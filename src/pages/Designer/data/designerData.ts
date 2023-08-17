aba4f098ee4d2793fdebe5c943cccd750b1681e0
seohee0112 <ghdtj7@naver.com>
2023-08-17 15:12:32 +0900

    docs : 디자이너 데이터 추가 및 타입 지정

---
+++ src/pages/Designer/data/designerData.ts
@@ -0,0 +1,84 @@
+import { DesingerInfo } from '~/types/designer';
+
+export const designerData: DesingerInfo[] = [
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+  {
+    name: '김나현',
+    track: ['UX', 'DF', 'BX'],
+    img: '',
+  },
+];
