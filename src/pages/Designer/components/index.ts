014fd4dfb93133b880262134470a67903ce80daf
seohee0112 <ghdtj7@naver.com>
2023-08-17 18:16:17 +0900

    feat : 컴포넌트 하나로 export

--- src/pages/Designer/components/index.ts
+++ src/pages/Designer/components/index.ts
@@ -1,2 +1,4 @@
 export { default as DesignerContainer } from './DesignerContainer';
 export { default as DesignerCard } from './DesignerCard';
+export { default as Profile } from './Profile';
+export { default as GraduationWorkList } from './GraduationWorkList';
