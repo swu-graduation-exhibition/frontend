2191866c1fb4592e524e246ed065d5b1182860d6
seohee0112 <ghdtj7@naver.com>
2023-08-17 18:13:25 +0900

    feat : 디자이너 상세 페이지 router 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import Landing from './pages/Landing/Landing';
 import Project from './pages/ProjectPage/Project';
 import Home from './pages/Home/Home';
 import Designer from './pages/Designer/Designer';
+import DesignerDetail from './pages/Designer/DesignerDetail';
+import { DesignerContainer } from './pages/Designer/components';
 
 function Router() {
   return (
@@ -11,7 +13,10 @@ function Router() {
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/project/:category" element={<Project />} />
-        <Route path="/designer" element={<Designer />}></Route>
+        <Route path="/designer" element={<Designer />}>
+          <Route path="" element={<DesignerContainer />} />
+          <Route path=":id" element={<DesignerDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
