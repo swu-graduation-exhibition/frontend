4fc000ed24f056e173d574332fe8bbccbefeead9
seohee0112 <ghdtj7@naver.com>
2023-08-17 18:14:02 +0900

    style : 모바일 전용 폰트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -57,6 +57,7 @@ const fonts = {
   Body_04: FONT({ weight: 300, size: 3, lineHeight: 4 }),
   Mobile_Body_01: FONT({ weight: 400, size: 1.6, lineHeight: 2.5 }),
   Mobile_Body_02: FONT({ weight: 500, size: 1.5, lineHeight: 2.17 }),
+  Mobile_Body_04: FONT({ weight: 350, size: 1.4, lineHeight: 2 }),
   Mobile_Body_05: FONT({ weight: 300, size: 1.3, lineHeight: 1.8 }),
   Caption_01: FONT({ weight: 300, size: 2.5, lineHeight: 4 }),
   Caption_02: FONT({ weight: 400, size: 2.2, lineHeight: 3 }),
