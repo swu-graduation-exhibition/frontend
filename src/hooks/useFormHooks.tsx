2508e561a6d0df426a27fd42cec79e643ab9cd25
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-21 21:39:47 +0900

    codeReivew: 서희의 코드리뷰 반영

--- src/hooks/useFormHooks.tsx
+++ src/hooks/useFormHooks.tsx
@@ -1,32 +1,24 @@
 import React, { useState } from 'react';
-
-interface IFormContents {
-  to: string;
-  message: string;
-}
+import { FormContents } from '~/types/commonFormSection';
 
 const useFormHooks = () => {
-  const [formData, setSetFormData] = useState<IFormContents>({
+  const [formData, setSetFormData] = useState<FormContents>({
     to: '',
     message: '',
   });
 
   const inputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length < 8) {
-      setSetFormData((prev) => {
-        return { ...prev, to: e.target.value };
-      });
+      setSetFormData((prev) => ({ ...prev, to: e.target.value }));
     }
   };
   const textAreaOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     if (e.target.value.length < 101) {
-      setSetFormData((prev) => {
-        return { ...prev, message: e.target.value };
-      });
+      setSetFormData((prev) => ({ ...prev, message: e.target.value }));
     }
   };
 
-  const isButtonActive = formData.to.length > 0 && formData.message.length > 0;
+  const isButtonActive = formData.to.length && formData.message.length;
   return { formData, isButtonActive, inputOnChange, textAreaOnChange };
 };
 
