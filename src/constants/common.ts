a07cf5a8e192e0af37d5369df72bb2a72ee923ec
seohee0112 <ghdtj7@naver.com>
2023-08-22 12:45:48 +0900

    fix : route 수정

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,7 +1,7 @@
 export const PAGE_CATEGORIES = ['PROJECT', 'DESIGNER', 'GUESTBOOK'] as const;
 export const MOBILE_PAGE_CATEGORIES = ['Project', 'Designer', 'Guest Book'] as const;
-export const PAGE_PATHS = ['/project', '/designer', '/guestbook'] as const;
-export const MOBILE_PAGE_PATHS = ['/project', '/designer', '/guestbook'] as const;
+export const PAGE_PATHS = ['/project/ux-design', '/designer', '/guestbook'] as const;
+export const MOBILE_PAGE_PATHS = ['/project/ux-design', '/designer', '/guestbook'] as const;
 
 export const MOBILE_WIDTH = `520px`;
 export const TABLET_WIDTH = `830px`;
