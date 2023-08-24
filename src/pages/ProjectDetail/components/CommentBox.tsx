37284d3f44bfed0989434c248ad1b5230a0a152c
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-24 20:52:23 +0900

    design: CommentBox Publishing

---
+++ src/pages/ProjectDetail/components/CommentBox.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+const Container = styled.article<{ lastElement: boolean }>`
+  display: flex;
+  flex-direction: row;
+  padding: 5.6rem 4.8rem;
+
+  border-bottom: ${({ lastElement, theme }) =>
+    lastElement ? `1px solid ${theme.colors.Grayscales_400}` : null};
+`;
+
+const CommentInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 18.3rem;
+  min-width: 18.3rem;
+`;
+
+const Name = styled.span(({ theme }) => theme.fonts.Subtitle_04);
+const Date = styled.span(
+  ({ theme }) => theme.fonts.Caption_03,
+  css`
+    color: ${({ theme }) => theme.colors.Grayscales_500};
+  `,
+);
+
+const Content = styled.span(({ theme }) => theme.fonts.Caption_03);
+
+interface CommentData {
+  from: string;
+  date: string;
+  content: string;
+}
+
+interface CommnetBoxProps {
+  commentData: CommentData;
+  lastElement: boolean;
+}
+
+const CommentBox = ({ commentData, lastElement }: CommnetBoxProps) => {
+  const { from, date, content } = commentData;
+  return (
+    <Container lastElement={lastElement}>
+      <CommentInfoBox>
+        <Name>{from}</Name>
+        <Date>{date}</Date>
+      </CommentInfoBox>
+      <Content>{content}</Content>
+    </Container>
+  );
+};
+
+export default CommentBox;
