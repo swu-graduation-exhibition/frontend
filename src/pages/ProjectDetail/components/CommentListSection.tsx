84b5b0e6d389b9e66b878de87c3fce884849d99e
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-26 23:16:20 +0900

    design: Pagination 기본 틀 Pbulishing

--- src/pages/ProjectDetail/components/CommentListSection.tsx
+++ src/pages/ProjectDetail/components/CommentListSection.tsx
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 import CommentBox from './CommentBox';
 import { commentList } from '../data/commentData';
+import Pagination from './Pagination';
 
 const CommentListSection = () => {
   return (
     <Container>
-      {commentList.map((commentData, i) => (
-        <CommentBox
-          key={i}
-          commentData={commentData}
-          lastElement={commentList.length - 1 === i ? false : true}
-        />
-      ))}
+      <CommentListWrapper>
+        {commentList.map((commentData, i) => (
+          <CommentBox
+            key={i}
+            commentData={commentData}
+            lastElement={commentList.length - 1 === i ? false : true}
+          />
+        ))}
+      </CommentListWrapper>
+      <Pagination />
     </Container>
   );
 };
@@ -20,4 +24,7 @@ export default CommentListSection;
 
 const Container = styled.div`
   height: fit-content;
+`;
+const CommentListWrapper = styled.section`
+  margin-bottom: 7.2rem;
 `;
