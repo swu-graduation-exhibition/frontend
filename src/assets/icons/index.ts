aed60fd5fb925423fb28b569c890c379478c3ffb
Chanwoo-Jeong <lordroad38@naver.com>
2023-08-26 18:40:23 +0900

    assets: pagination 좌 우 화살표 svg 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,4 +3,6 @@ export { ReactComponent as IcDefaultBar } from './ic_default_bar.svg';
 export { ReactComponent as IcCloseBar } from './x.svg';
 export { ReactComponent as IcArrowHome } from './ic_arrow_home.svg';
 export { ReactComponent as IcStarAll } from './ic_star_all.svg';
+export { ReactComponent as IcArrowLeft } from './ic_arrow_left.svg';
+export { ReactComponent as IcArrowRight } from './ic_arrow_right.svg';
 
