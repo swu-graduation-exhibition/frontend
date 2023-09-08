6cfd65314e3bfe8d7a42b863d1a9382effaaafcb
seohee0112 <ghdtj7@naver.com>
2023-09-08 22:33:09 +0900

    fix : 반응형 분기점 상수 변경

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,6 +3,6 @@ export const MOBILE_PAGE_CATEGORIES = ['Project', 'Designer', 'Guest Book'] as c
 export const PAGE_PATHS = ['/project/ux-design', '/designer', '/guestbook'] as const;
 export const MOBILE_PAGE_PATHS = ['/project/ux-design', '/designer', '/guestbook'] as const;
 
-export const MOBILE_WIDTH = `520px`;
-export const TABLET_WIDTH = `830px`;
+export const MOBILE_WIDTH = `521px`;
+export const TABLET_WIDTH = `831px`;
 export const DESKTOP_WIDTH = '1400px';
