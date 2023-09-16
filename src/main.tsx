3fe228e4d9e304781dae277ea528ce5f8f5565c2
seohee0112 <ghdtj7@naver.com>
2023-09-16 19:03:56 +0900

    [ feat ] : 리액트 쿼리 허용 및 데브툴 서비스에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx';
 import { GlobalStyle } from './styles/globalStyle.ts';
 import theme from './styles/theme';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
