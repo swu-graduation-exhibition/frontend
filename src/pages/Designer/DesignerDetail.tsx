940a294c98996212faa4245e6cd8a67e423e8c1f
seohee0112 <ghdtj7@naver.com>
2023-09-16 20:37:03 +0900

    [ feat ] : 페이지네이션 적용하여 구현

--- src/pages/Designer/DesignerDetail.tsx
+++ src/pages/Designer/DesignerDetail.tsx
@@ -3,16 +3,34 @@ import { GraduationWorkList, Profile } from './components';
 import { MOBILE_WIDTH } from '~/constants/common';
 import CommentListSection from '../ProjectDetail/components/CommentListSection';
 import CommonFormSection from '~/common/components/CommonFormSection';
+import { guestBookCommentData } from '~/common/data/guestBookCommentList';
+import GuestBookCommentCard from '~/common/components/GuestBookCommentCard';
+import Pagination from '../ProjectDetail/components/Pagination';
+import { useState } from 'react';
 
 const DesignerDetail = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const lastPage = Math.ceil(100 / 8);
+  const paginationNumbers = Array.from({ length: lastPage }).map((_, i) => i + 1);
+
   return (
     <DesignerDetailWrapper>
       <Profile />
       <GraduationWorkList />
       <GuestBookWrapper>
         <CommonFormSection page="designer" />
-        <CommentListSection />
       </GuestBookWrapper>
+      <CommentListWrapper>
+        {guestBookCommentData.map(({ sender, content, createdAt }, idx) => (
+          <GuestBookCommentCard key={idx} sender={sender} content={content} createdAt={createdAt} />
+        ))}
+      </CommentListWrapper>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        lastPage={lastPage}
+        paginationNumbers={paginationNumbers}
+      />
     </DesignerDetailWrapper>
   );
 };
@@ -36,4 +54,14 @@ const DesignerDetailWrapper = styled.div`
 
 const GuestBookWrapper = styled.div`
   margin-top: 20rem;
+`;
+
+const CommentListWrapper = styled.div`
+  display: grid;
+  max-width: 165rem;
+  grid-template-columns: repeat(4, minmax(27rem, 1fr));
+  gap: 2rem;
+
+  margin-top: 12rem;
+  margin-bottom: 10rem;
 `;
