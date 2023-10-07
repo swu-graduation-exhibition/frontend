ad8828910b25b23480ad727f3567d2663324b112
Seojisoo20191941 <amy0480@naver.com>
2023-10-07 14:34:33 +0900

    [ etc ] css 수정

--- src/common/components/PageLayout.tsx
+++ src/common/components/PageLayout.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 import { styled } from 'styled-components';
+import ImgDesignerBackgroundCir from '~/assets/images/DesignerBgCir.png';
 import ImgMobileGraduationExhibition from '~/assets/images/bg1_all_mobile.png';
 import ImgDesignerBackground from '~/assets/images/bg1_default_designer.png';
 import desktopBackground from '~/assets/images/guestBookBackgroundImage.png';
 import mobileBackground from '~/assets/images/guestBookMobileBackgroundImage.png';
+import ImgGraduationExhibition from '~/assets/images/home_middle_background.png';
 import { Default, Mobile } from '~/utils/mediaQuery';
 import { Footer, Header } from '.';
-import ImgGraduationExhibition from '~/assets/images/home_middle_background.png';
-import ImgDesignerBackgroundCir from '~/assets/images/DesignerBgCir.png';
 
 interface PageLayoutProps {
   mainBanner?: ReactNode;
@@ -17,7 +17,6 @@ interface PageLayoutProps {
 
 const PageLayout = ({ children, mainBanner }: PageLayoutProps) => {
   const { pathname } = useLocation();
-  console.log(pathname);
 
   return (
     <PageLayoutWrapper>
@@ -27,26 +26,6 @@ const PageLayout = ({ children, mainBanner }: PageLayoutProps) => {
         {children}
         <Footer />
         <Default>
-<<<<<<< HEAD
-          <PageContentImg
-            src={
-              pathname === '/designer' || pathname === '/project/detail'
-                ? ImgDesignerBackground
-                : pathname === '/guestbook'
-                ? desktopBackground
-                : ImgGraduationExhibition
-            }
-          />
-        </Default>
-        <Mobile>
-          <PageContentImg
-            src={
-              pathname === '/guestbook' || pathname === '/project/detail'
-                ? mobileBackground
-                : ImgMobileGraduationExhibition
-            }
-          />
-=======
           <>
             {pathname === '/home' && <PageContentAbosoulteImg src={ImgGraduationExhibition} />}
             {pathname.includes('/designer') && <PageContentImg src={ImgDesignerBackground} />}
@@ -66,7 +45,6 @@ const PageLayout = ({ children, mainBanner }: PageLayoutProps) => {
               </>
             )}
           </>
->>>>>>> 85cf62eeb964d81bea17452b613026f3b1176031
         </Mobile>
       </ContentWrapper>
     </PageLayoutWrapper>
