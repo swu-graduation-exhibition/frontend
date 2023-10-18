5ce51adef4894a94ad240645d4e40a883c683de5
seohee0112 <ghdtj7@naver.com>
2023-10-18 23:59:52 +0900

    fix : ㄹㅣ코일 삭제 및 필요없는 script 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RecoilRoot } from 'recoil';
 import Router from './Router';
 
 const App = () => {
   return (
-    <RecoilRoot>
+    <>
       <Router />
       <ReactQueryDevtools initialIsOpen={false} />
-    </RecoilRoot>
+    </>
   );
 };
 
