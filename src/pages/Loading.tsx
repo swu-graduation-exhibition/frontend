8ca74fbcef7a09ad451acda10f14074b0e49eff4
Seojisoo20191941 <amy0480@naver.com>
2023-10-26 20:04:53 +0900

    [ feat ] 로딩 배경 제거

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -15,7 +15,8 @@ const Background = styled.div`
   position: fixed;
   inset: 0;
 
-  background-color: rgb(0 0 0 / 40%);
+  /* background-color: rgb(0 0 0 / 40%); */
+
   flex-direction: column;
 `;
 
