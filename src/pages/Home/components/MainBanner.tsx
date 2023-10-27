8eee397714e0d1f71b645b930015910c670ff5d2
happhee <ghdtj7@naver.com>
2023-10-27 11:14:30 +0900

    fix : 배너 이미지 수정

--- src/pages/Home/components/MainBanner.tsx
+++ src/pages/Home/components/MainBanner.tsx
@@ -6,23 +6,13 @@ const MainBanner = () => {
   return (
     <>
       <HomeDesktop>
-        <Wrapper>
-          <MainBannerWrapper src={`${import.meta.env.VITE_SWU_IMAGE}/img1_home.png`} />
-          <UniversityContent />
-          <LocationContent />
-        </Wrapper>
+        <MainImg src={`${import.meta.env.VITE_SWU_IMAGE}/banner_home_pc.png`} />
       </HomeDesktop>
       <HomeTablet>
-        <TabletBannerWrapper>
-          <UniversityContent />
-          <LocationContent />
-        </TabletBannerWrapper>
+        <MainImg src={`${import.meta.env.VITE_SWU_IMAGE}/banner_home_tab.png`} />
       </HomeTablet>
       <HomeMobile>
-        <MobileBannerWrapper>
-          <UniversityContent />
-          <LocationContent />
-        </MobileBannerWrapper>
+        <MainImg src={`${import.meta.env.VITE_SWU_IMAGE}/banner_home_mobile.jpg`} />
       </HomeMobile>
     </>
   );
@@ -30,46 +20,8 @@ const MainBanner = () => {
 
 export default MainBanner;
 
-const Wrapper = styled.div`
-  position: relative;
+const MainImg = styled.img`
   width: 100%;
-  max-height: 974px;
-
-  /* border-bottom: 1px solid black; */
-`;
-const MainBannerWrapper = styled.img`
-  /* margin: 6rem 5% 10rem 5%; */
-
-  width: 102%;
-
-  /* max-width: 1931px;
-  max-height: 1018px; */
 
   object-fit: cover;
-`;
-const MobileBannerWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 52.9rem;
-  padding: 4% 3%;
-  padding-top: calc(529 / 374 * 100%);
-
-  background-image: url(${`${import.meta.env.VITE_SWU_IMAGE}/img1_home_mobile.png`});
-  background-position: center center;
-  background-size: cover;
-  background-repeat: no-repeat;
-`;
-const TabletBannerWrapper = styled.div`
-  position: relative;
-  width: 100%;
-
-  /* height: 72rem; */
-
-  /* padding: 6rem 5% 10rem; */
-
-  padding-top: calc(720 / 830 * 100%);
-  background-repeat: no-repeat;
-  background-size: 100%;
-  background-position: center center;
-  background-image: url(${`${import.meta.env.VITE_SWU_IMAGE}/img1_home_pad.png`});
 `;
