11ac078cc42c9c3a7100cf401002b963b48fd169
Seojisoo20191941 <amy0480@naver.com>
2023-10-28 17:42:39 +0900

    [ fix ] 개행 가능하도록 수정

--- src/pages/ProjectDetail/components/ProjectDetailHeader.tsx
+++ src/pages/ProjectDetail/components/ProjectDetailHeader.tsx
@@ -100,6 +100,8 @@ const ProjectName = styled.div(
       margin-right: 0.5rem;
     }
 
+    white-space: pre;
+
     @media screen and (width <= ${`1100px`}) {
       ${({ theme }) => theme.fonts.Subtitle_02}
     }
@@ -157,6 +159,8 @@ const Description = styled.div(
   ({ theme }) => theme.fonts.Caption_01,
   css`
     color: ${({ theme }) => theme.colors.Grayscales_900};
+
+    white-space: pre;
 
     @media screen and (width <= ${MOBILE_WIDTH}) {
       ${({ theme }) => theme.fonts.Mobile_Caption_01}
